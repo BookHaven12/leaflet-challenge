614df793f285b00eac71b45eeaa7329633a51fa2
BookHaven12 <iyermegan@gmail.com>
2025-03-31 13:03:29 -0700

    fixed errors in the code for magnitutde and color

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,7 +15,7 @@ let baseMaps = {
 let map = L.map("map-id", {
   center: [40.73, -74.0059],
   zoom: 12,
-  layers: [streetmap, bikeStations]
+  layers: [streetmap]
 });
 
 // Then add the 'basemap' tile layer to the map.
@@ -40,9 +40,9 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // This function determines the color of the marker based on the depth of the earthquake.
   function getColor(depth) {
     if (depth > 90) return "red";
-    else if (depth > 70) retrun "orangered";
-    else if (depth > 50) retrun "orange";
-    else if (depth > 30) return "gold";
+    else if (depth > 70) return "orangered";
+    else if (depth > 50) return "orange";
+    else if (depth > 30) return "yellow";
     else if (depth > 10) return "yellowgreen";
     else return "green";
 
@@ -50,7 +50,8 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
   // This function determines the radius of the earthquake marker based on its magnitude.
   function getRadius(magnitude) {
-
+    if (magnitude == 0) return 1;
+    else return magnitude * 10;
   }
 
   // Add a GeoJSON layer to the map once the file is loaded.
